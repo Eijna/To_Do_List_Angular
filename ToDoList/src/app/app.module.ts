b22c9dac37ad7c4324f8d089d5ff38b1193b5faa
Angie <angelacruz.pro@gmail.com>
2022-08-19 14:16:24 +0200

    Refactoring, ajout de FormsModule (NgModel)

--- ToDoList/src/app/app.module.ts
+++ ToDoList/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <- NgModel
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TasksComponent
+    TasksComponent,
+    TaskDetailComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
